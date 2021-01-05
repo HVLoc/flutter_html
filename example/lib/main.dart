19186245673ca7af62413e582bcf4a633b26c593
lochv <lochv@intern.softdreams.vn>
2021-01-05 08:11:11 +0700

    change example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,111 +29,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 const htmlData = """
-<h1>Header 1</h1>
-<h2>Header 2</h2>
-<h3>Header 3</h3>
-<h4>Header 4</h4>
-<h5>Header 5</h5>
-<h6>Header 6</h6>
-<h3>Ruby Support:</h3>
-      <p>
-        <ruby>
-          漢<rt>かん</rt>
-          字<rt>じ</rt>
-        </ruby>
-        &nbsp;is Japanese Kanji.
-      </p>
-      <h3>Support for <code>sub</code>/<code>sup</code></h3>
-      Solve for <var>x<sub>n</sub></var>: log<sub>2</sub>(<var>x</var><sup>2</sup>+<var>n</var>) = 9<sup>3</sup>
-      <p>One of the most <span>common</span> equations in all of physics is <br /><var>E</var>=<var>m</var><var>c</var><sup>2</sup>.</p>
-      <h3>Inline Styles:</h3>
-      <p>The should be <span style='color: blue;'>BLUE style='color: blue;'</span></p>
-      <p>The should be <span style='color: red;'>RED style='color: red;'</span></p>
-      <p>The should be <span style='color: rgba(0, 0, 0, 0.10);'>BLACK with 10% alpha style='color: rgba(0, 0, 0, 0.10);</span></p>
-      <p>The should be <span style='color: rgb(0, 97, 0);'>GREEN style='color: rgb(0, 97, 0);</span></p>
-      <p>The should be <span style='background-color: red; color: rgb(0, 97, 0);'>GREEN style='color: rgb(0, 97, 0);</span></p>
-      <p style="text-align: center;"><span style="color: rgba(0, 0, 0, 0.95);">blasdafjklasdlkjfkl</span></p>
-      <p style="text-align: right;"><span style="color: rgba(0, 0, 0, 0.95);">blasdafjklasdlkjfkl</span></p>
-      <p style="text-align: justify;"><span style="color: rgba(0, 0, 0, 0.95);">blasdafjklasdlkjfkl</span></p>
-      <p style="text-align: center;"><span style="color: rgba(0, 0, 0, 0.95);">blasdafjklasdlkjfkl</span></p>
-      <h3>Table support (with custom styling!):</h3>
-      <p>
-      <q>Famous quote...</q>
-      </p>
-      <table>
-      <colgroup>
-        <col width="50%" />
-        <col width="25%" />
-        <col width="25%" />
-      </colgroup>
-      <thead>
-      <tr><th>One</th><th>Two</th><th>Three</th></tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td rowspan='2'>Rowspan\nRowspan\nRowspan\nRowspan\nRowspan\nRowspan\nRowspan\nRowspan\nRowspan\nRowspan</td><td>Data</td><td>Data</td>
-      </tr>
-      <tr>
-        <td colspan="2"><img alt='Google' src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png' /></td>
-      </tr>
-      </tbody>
-      <tfoot>
-      <tr><td>fData</td><td>fData</td><td>fData</td></tr>
-      </tfoot>
-      </table>
-      <h3>Custom Element Support:</h3>
-      <flutter></flutter>
-      <flutter horizontal></flutter>
-      <h3>SVG support:</h3>
-      <svg id='svg1' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
-            <circle r="32" cx="35" cy="65" fill="#F00" opacity="0.5"/>
-            <circle r="32" cx="65" cy="65" fill="#0F0" opacity="0.5"/>
-            <circle r="32" cx="50" cy="35" fill="#00F" opacity="0.5"/>
-      </svg>
-      <h3>List support:</h3>
-      <ol>
-            <li>This</li>
-            <li><p>is</p></li>
-            <li>an</li>
-            <li>
-            ordered
-            <ul>
-            <li>With<br /><br />...</li>
-            <li>a</li>
-            <li>nested</li>
-            <li>unordered
-            <ol>
-            <li>With a nested</li>
-            <li>ordered list.</li>
-            </ol>
-            </li>
-            <li>list</li>
-            </ul>
-            </li>
-            <li>list! Lorem ipsum dolor sit amet.</li>
-            <li><h2>Header 2</h2></li>
-            <h2><li>Header 2</li></h2>
-      </ol>
-      <h3>Link support:</h3>
-      <p>
-        Linking to <a href='https://github.com'>websites</a> has never been easier.
-      </p>
-      <h3>Image support:</h3>
-      <p>
-        <img alt='Google' src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png' />
-        <a href='https://google.com'><img alt='Google' src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png' /></a>
-        <img alt='Alt Text of an intentionally broken image' src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30d' />
-      </p>
-      <h3>Video support:</h3>
-      <video controls>
-        <source src="https://www.w3schools.com/html/mov_bbb.mp4" />
-      </video>
-      <h3>Audio support:</h3>
-      <audio controls>
-        <source src="https://www.w3schools.com/html/mov_bbb.mp4" />
-      </audio>
-      <h3>IFrame support:</h3>
-      <iframe src="https://google.com"></iframe>
+<table style="height: 356px; width: 100%; border-collapse: collapse;" border="1">
+<tbody>
+<tr style="height: 56px;">
+<td style="width: 24.4615%; height: 34px; text-align: center;" rowspan="2" width="156">Loại giấy tờ</td>
+<td style="width: 103.385%; height: 10px; text-align: center;" colspan="3" width="467">
+<p style="text-align: center;">Thời hạn nộp (Hạn cuối)</p>
+</td>
+</tr>
+<tr style="height: 24px;">
+<td style="text-align: center; width: 24.4615%; height: 24px;" width="156">Theo tháng</td>
+<td style="text-align: center; width: 24.6154%; height: 24px;" width="156">Theo quý</td>
+<td style="text-align: center; width: 54.3077%; height: 24px;" width="156">Theo năm</td>
+</tr>
+<tr style="height: 24px;">
+<td style="width: 24.4615%; height: 24px; text-align: center;" width="156"><strong>Thuế môn bài</strong></td>
+<td style="width: 24.4615%; height: 24px; text-align: center;" width="156"></td>
+<td style="width: 24.6154%; height: 24px; text-align: center;" width="156"></td>
+<td style="text-align: center; width: 54.3077%; height: 24px;" width="156">30/1</td>
+</tr>
+<tr style="height: 48px;">
+<td style="width: 24.4615%; height: 48px; text-align: center;" width="156"><strong>Thuế GTGT</strong></td>
+<td style="width: 24.4615%; height: 48px; text-align: center;" width="156">Ngày 20 của tháng sau</td>
+<td style="width: 24.6154%; height: 48px; text-align: center;" width="156">Ngày 30 của tháng đầu quý sau</td>
+<td style="width: 54.3077%; height: 48px; text-align: center;" width="156"></td>
+</tr>
+<tr style="height: 48px;">
+<td style="width: 24.4615%; height: 48px; text-align: center;" width="156"><strong>Thuế TNCN</strong></td>
+<td style="width: 24.4615%; height: 48px; text-align: center;" width="156">Ngày 20 của tháng sau</td>
+<td style="width: 24.6154%; height: 48px; text-align: center;" width="156">Ngày 30 của tháng đầu quý sau</td>
+<td style="width: 54.3077%; height: 48px; text-align: center;" width="156"></td>
+</tr>
+<tr style="height: 10px;">
+<td style="width: 24.4615%; height: 10px; text-align: center;" width="156"><strong>Thuế TNDN</strong></td>
+<td style="width: 24.4615%; height: 10px; text-align: center;" width="156"></td>
+<td style="width: 24.6154%; height: 10px; text-align: center;" width="156">Chỉ phải nộp số tiền tạm tính</p>
+<p>Không phải nộp tờ khai</td>
+<td style="width: 54.3077%; height: 10px; text-align: center;" width="156"></td>
+</tr>
+<tr style="height: 48px;">
+<td style="width: 24.4615%; height: 48px; text-align: center;" width="156"><strong>Báo cáo tình hình sử dụng hóa đơn</strong></td>
+<td style="width: 24.4615%; height: 48px; text-align: center;" width="156">Ngày 20 của tháng sau</td>
+<td style="width: 24.6154%; height: 48px; text-align: center;" width="156">Ngày 30 của tháng đầu quý sau</td>
+<td style="width: 54.3077%; height: 48px; text-align: center;" width="156"></td>
+</tr>
+<tr style="height: 144px;">
+<td style="width: 24.4615%; height: 144px; text-align: center;" width="156"><strong>Báo cáo tài chính &amp; Quyết toán thuế TNDN, TNCN</strong></td>
+<td style="width: 24.4615%; height: 144px; text-align: center;" width="156"></td>
+<td style="width: 24.6154%; height: 144px; text-align: center;" width="156"></td>
+<td style="width: 54.3077%; height: 144px; text-align: center;" width="156">Trong vòng 90 ngày kể từ ngày kết thúc năm dương lịch hoặc năm tài chính</p>
+<p>(Hạn cuối là 30 hoặc 31/3)</td>
+</tr>
+</tbody>
+</table>
 """;
 
 class _MyHomePageState extends State<MyHomePage> {
